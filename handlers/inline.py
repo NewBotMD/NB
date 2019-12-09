01fd4ea195bb740b5c1a1d58b273c4f513d3c0ab
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-09 13:36:07 +0300

    Create inline.py

---
+++ handlers/inline.py
@@ -0,0 +1,30 @@
+from utlis.rank import setrank,isrank,remrank,remsudos,setsudo,GPranks,IDrank
+from utlis.send import send_msg, BYusers, Sendto, fwdto,Name,Glang
+from utlis.locks import st,getOR,Clang,st_res
+from utlis.tg import Bot
+from config import *
+
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+import threading, requests, time, random, re, json,datetime
+import importlib
+
+
+from os import listdir
+from os.path import isfile, join
+
+def updateInline(client, inline_query,redis):
+  print("inline")
+
+  if redis.smembers("{}Nbot:botfiles".format(BOT_ID)):
+    onlyfiles = [f for f in listdir("files") if isfile(join("files", f))]
+    filesR = redis.smembers("{}Nbot:botfiles".format(BOT_ID))
+    for f in onlyfiles:
+      if f in filesR:
+        fi = f.replace(".py","")
+        UpMs= "files."+fi
+        try:
+          U = importlib.import_module(UpMs)
+          U.updateIn(client, inline_query,redis)
+        except Exception as e:
+          pass
+
